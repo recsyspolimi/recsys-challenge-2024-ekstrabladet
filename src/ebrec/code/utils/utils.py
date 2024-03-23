381b36789488e5bf8f42ef43256fa57268fbc34c
FrancescoZanella <francescozanella00@gmail.com>
2024-03-23 16:42:19 +0100

    added import

--- src/ebrec/code/utils/utils.py
+++ src/ebrec/code/utils/utils.py
@@ -3,8 +3,10 @@ import pathlib as Path
 
 from src.ebrec.code.utils.constants import PATH
 
+
 def load_history_behaviors(bundle = "large", split = "train", filename = "behaviors"):
     print("I'm loading {} from {}, bundle: {}".format(filename,split, bundle))
+    print(PATH)
     return pl.read_parquet(Path(str(PATH) + bundle).joinpath(split,filename+".parquet"))
 
 
