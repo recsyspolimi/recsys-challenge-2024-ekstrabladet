999ae7ed51fa7d96e881a00841f0cc26a4be05c1
Carlo Sgaravatti <carlo.sgaravatti@gmail.com>
2024-04-04 21:22:02 +0200

    prepared scripts for preprocessing and training on the server

---
+++ src/polimi/scripts/run_training.sh
@@ -0,0 +1,5 @@
+python ./training.py \
+    -output_dir /media/disk1/recsys-challenge-2024/experiments \
+    -dataset_path /path/to/preprocessed/train_ds.parquet \
+    -catboost_params_file /media/disk1/recsys-challenge-2024/RecsysChallenge2024/configuration_files/catboost_params.json \
+    -catboost_verbosity 50
