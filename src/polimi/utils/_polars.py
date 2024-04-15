e640104e02696f62ae817c99f00f32c211dad3ce
lorecampa <lorenzo.campana@mail.polimi.it>
2024-04-15 23:39:41 +0200

    reduce size also of list elements

--- src/polimi/utils/_polars.py
+++ src/polimi/utils/_polars.py
@@ -6,13 +6,12 @@ import gc
 
 
 def reduce_polars_df_memory_size(df: pl.DataFrame):
-
     start_mem = df.estimated_size('mb')
     print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
     
     for col in df.columns:
         col_type = df[col].dtype
-        
+        # Integer types
         if col_type in [pl.Int16, pl.Int32, pl.Int64]:
             c_min = df[col].fill_null(0).min()
             c_max = df[col].fill_null(0).max()
@@ -31,11 +30,31 @@ def reduce_polars_df_memory_size(df: pl.DataFrame):
                 df = df.with_columns(pl.col(col).cast(pl.UInt16))
             elif c_min > np.iinfo(np.uint32).min and c_max < np.iinfo(np.uint32).max:
                 df = df.with_columns(pl.col(col).cast(pl.UInt32))
+        # Float types
         elif col_type == pl.Float64:
             c_min = df[col].fill_null(0).min()
             c_max = df[col].fill_null(0).max()
             if c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
                 df = df.with_columns(pl.col(col).cast(pl.Float32))
+        # List types
+        elif col_type in [pl.List(pl.Int16), pl.List(pl.Int32), pl.List(pl.Int64)]:
+            c_min = df[col].list.min().min()
+            c_max = df[col].list.max().max()
+            if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                df = df.with_columns(pl.col(col).cast(pl.List(pl.Int8)))
+            elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                df = df.with_columns(pl.col(col).cast(pl.List(pl.Int16)))
+            elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                df = df.with_columns(pl.col(col).cast(pl.List(pl.Int32)))
+        elif col_type in [pl.List(pl.UInt16), pl.List(pl.UInt32), pl.List(pl.UInt64)]:
+            c_min = df[col].list.min().min()
+            c_max = df[col].list.max().max()
+            if c_min > np.iinfo(np.uint8).min and c_max < np.iinfo(np.uint8).max:
+                df = df.with_columns(pl.col(col).cast(pl.List(pl.UInt8)))
+            elif c_min > np.iinfo(np.uint16).min and c_max < np.iinfo(np.uint16).max:
+                df = df.with_columns(pl.col(col).cast(pl.List(pl.UInt16)))
+            elif c_min > np.iinfo(np.uint32).min and c_max < np.iinfo(np.uint32).max:
+                df = df.with_columns(pl.col(col).cast(pl.List(pl.UInt32)))
 
     gc.collect()
     end_mem = df.estimated_size('mb')
