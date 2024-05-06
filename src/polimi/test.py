dfd1cc0d36108c559e0a9df065e5b5e2f00dd852
lorecampa <andrea.alari@mail.polimi.it>
2024-05-06 18:21:44 +0000

    lightgbm config files

--- src/polimi/test.py
+++
@@ -1,6 +0,0 @@
-import polars as pl
-from polimi.utils._catboost import _preprocessing_article_endorsement_feature
-from tqdm import tqdm
-
-if __name__ == '__main__': 
-    print(pl.read_parquet('/home/ubuntu/experiments/preprocessing_train_2024-04-22_13-28-45/train_ds.parquet').head())
