a47eda75b8e15af1c8ec2ae56f2bfa5bf22b25ac
CarloSgaravatti <carlo.sgaravatti@mail.polimi.it>
2024-05-09 14:14:59 +0000

    refactor script for training catboost

--- src/polimi/scripts/run_catboost_training.sh
+++ src/polimi/scripts/run_catboost_training.sh
@@ -1,4 +1,4 @@
-python ~/RecSysChallenge2024/src/polimi/scripts/training.py \
+python ~/RecSysChallenge2024/src/polimi/scripts/catboost_training.py \
     -output_dir /home/ubuntu/experiments \
     -dataset_path /home/ubuntu/experiments/preprocessing_train_2024-04-10_18-33-48 \
     -catboost_params_file /home/ubuntu/RecSysChallenge2024/configuration_files/catboost_params.json \
