f7ba1c7e4b055d8eb72c6e6fbf03105d5ffb647d
lorecampa <francescozanella00@gmail.com>
2024-05-17 09:41:32 +0000

    fixed script build_recsys_scores

--- src/polimi/scripts/run_build_recsys_scores.sh
+++ src/polimi/scripts/run_build_recsys_scores.sh
@@ -2,8 +2,22 @@ python ~/RecSysChallenge2024/src/polimi/scripts/build_recsys_scores_features.py 
     -dataset_split train \
     -dataset_type large \
     -urm_type recsys \
-    -base_path /mnt/ebs_volume/recsys2024/\
-    -output_dir /mnt/ebs_volume/recsys2024/algo/features/\
+    -base_path /mnt/ebs_volume_2/recsys2024/\
+    -output_dir /mnt/ebs_volume_2/recsys2024/features/\
+
+python ~/RecSysChallenge2024/src/polimi/scripts/build_recsys_scores_features.py \
+    -dataset_split validation \
+    -dataset_type large \
+    -urm_type recsys \
+    -base_path /mnt/ebs_volume_2/recsys2024/\
+    -output_dir /mnt/ebs_volume_2/recsys2024/features/\
+
+python ~/RecSysChallenge2024/src/polimi/scripts/build_recsys_scores_features.py \
+    -dataset_split test \
+    -dataset_type testset \
+    -urm_type recsys \
+    -base_path /mnt/ebs_volume_2/recsys2024/\
+    -output_dir /mnt/ebs_volume_2/recsys2024/features/\
     
     
 
