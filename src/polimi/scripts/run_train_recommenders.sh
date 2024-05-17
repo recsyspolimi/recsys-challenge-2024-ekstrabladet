c125861e71f6c036864fee3c85af95e4c2e9e0aa
lorecampa <francescozanella00@gmail.com>
2024-05-17 09:39:59 +0000

    fixed script training recs

--- src/polimi/scripts/run_train_recommenders.sh
+++ src/polimi/scripts/run_train_recommenders.sh
@@ -1,5 +1,5 @@
 python ~/RecSysChallenge2024/src/polimi/scripts/train_recommenders.py \
-    -urm_path /mnt/ebs_volume_2/recsys2024/urm/recsys/small \
+    -urm_path /mnt/ebs_volume_2/recsys2024/urm/recsys/large \
     -urm_split train\
     -dataset_type large\
     -urm_type recsys\
@@ -7,7 +7,7 @@ python ~/RecSysChallenge2024/src/polimi/scripts/train_recommenders.py \
     -output_dir /mnt/ebs_volume_2/recsys2024/algo/\
 
 python ~/RecSysChallenge2024/src/polimi/scripts/train_recommenders.py \
-    -urm_path /mnt/ebs_volume_2/recsys2024/urm/recsys/small \
+    -urm_path /mnt/ebs_volume_2/recsys2024/urm/recsys/large \
     -urm_split validation\
     -dataset_type large\
     -urm_type recsys\
@@ -15,7 +15,7 @@ python ~/RecSysChallenge2024/src/polimi/scripts/train_recommenders.py \
     -output_dir /mnt/ebs_volume_2/recsys2024/algo/\
 
 python ~/RecSysChallenge2024/src/polimi/scripts/train_recommenders.py \
-    -urm_path /mnt/ebs_volume_2/recsys2024/urm/recsys/small \
+    -urm_path /mnt/ebs_volume_2/recsys2024/urm/recsys/testset \
     -urm_split test\
     -dataset_type testset\
     -urm_type recsys\
