4c3199ddf449be18106bb18c716af790bedf9071
CarloSgaravatti <carlo.sgaravatti@mail.polimi.it>
2024-06-05 00:06:18 +0000

    .

--- src/polimi/scripts/nn_training.py
+++ src/polimi/scripts/nn_training.py
@@ -11,6 +11,7 @@ from typing_extensions import List, Tuple, Dict, Type, TypeVar
 import polars as pl
 import gc
 import tensorflow as tf
+import joblib
 
 from polimi.utils.tf_models import *
 from polimi.utils.tf_models.utils import *
@@ -32,11 +33,11 @@ def get_model_class(name: str = 'mlp') -> T:
         return GANDALF
 
 
-def main(dataset_path, params_path, output_dir, early_stopping_path, es_patience):
+def main(dataset_path, params_path, output_dir, early_stopping_path, es_patience, transform_path):
     logging.info(f"Loading the preprocessed dataset from {dataset_path}")
     
     train_ds = pl.read_parquet(os.path.join(dataset_path, 'train_ds.parquet'))
-    val_ds = pl.read_parquet(os.path.join(dataset_path, 'train_ds.parquet')) if early_stopping_path else None
+    val_ds = pl.read_parquet(os.path.join(early_stopping_path, 'validation_ds.parquet')) if early_stopping_path else None
     with open(os.path.join(dataset_path, 'data_info.json')) as data_info_file:
         data_info = json.load(data_info_file)
         
@@ -65,6 +66,10 @@ def main(dataset_path, params_path, output_dir, early_stopping_path, es_patience
     if early_stopping_path:
         val_ds = val_ds.drop(['impression_id', 'article', 'user_id']).to_pandas()
         val_ds[data_info['categorical_columns']] = val_ds[data_info['categorical_columns']].astype('category')
+        if transform_path:
+            xformer = joblib.load(transform_path)
+            val_ds[xformer.feature_names_in_] = xformer.transform(val_ds[xformer.feature_names_in_].replace(
+                [-np.inf, np.inf], np.nan).fillna(0)).astype(np.float32)
         validation_data = (val_ds[X.columns], val_ds['target'])
         del val_ds
         gc.collect()
@@ -101,6 +106,8 @@ def main(dataset_path, params_path, output_dir, early_stopping_path, es_patience
         early_stopping_monitor='val_auc',
         early_stopping_mode='max',
         early_stopping_rounds=es_patience,
+        save_checkpoints=True,
+        checkpoint_dir=os.path.join(output_dir, 'checkpoints')
     )
         
     logging.info(f'Model fitted. Saving the model and the feature importances at: {output_dir}')
@@ -121,6 +128,8 @@ if __name__ == '__main__':
                         help="The name of the model")
     parser.add_argument("-early_stopping_patience", default=2, type=int,
                         help="The patience for early stopping")
+    parser.add_argument("-numerical_transformer_es", default=None, type=str,
+                        help="The path for numerical transformer to transform the early stopping data if needed")
     
     args = parser.parse_args()
     OUTPUT_DIR = args.output_dir
@@ -128,6 +137,7 @@ if __name__ == '__main__':
     HYPERPARAMS_PATH = args.params_file
     EARLY_STOPPING_PATH = args.early_stopping_path
     EARLY_STOPPING_PATIENCE = args.early_stopping_patience
+    TRANSFORM_PATH = args.numerical_transformer_es
     model_name = args.model_name
     
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -145,4 +155,4 @@ if __name__ == '__main__':
     root_logger = logging.getLogger()
     root_logger.addHandler(stdout_handler)
     
-    main(DATASET_DIR, HYPERPARAMS_PATH, output_dir, EARLY_STOPPING_PATH, EARLY_STOPPING_PATIENCE)
+    main(DATASET_DIR, HYPERPARAMS_PATH, output_dir, EARLY_STOPPING_PATH, EARLY_STOPPING_PATIENCE, TRANSFORM_PATH)
