e0fdae0de46d2adc5b8c6304c9aa6f02dca2aa16
SaverioMaggese99 <126009586+SaverioMaggese99@users.noreply.github.com>
2024-06-05 17:52:29 +0200

    Uploaded files for AutoInt training

---
+++ src/polimi/scripts/run_autoint_training.sh
@@ -0,0 +1,5 @@
+python ~/RecSysChallenge2024/src/polimi/scripts/autoint_training.py \
+    -output_dir /mnt/ebs_volume/models \
+    -dataset_path /mnt/ebs_volume/experiments/subsample_train_new \
+    -params_file /home/ubuntu/RecSysChallenge2024/configuration_files/best_params_autoint.json \
+    -model_name AutoINT_new_features_
