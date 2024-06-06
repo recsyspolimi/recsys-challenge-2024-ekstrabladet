e54e0bc728319462e4edb04efa07cef80ded9413
SaverioMaggese99 <126009586+SaverioMaggese99@users.noreply.github.com>
2024-06-06 16:32:28 +0200

    uploaded files to train nfm

---
+++ src/polimi/scripts/run_nfm_training.sh
@@ -0,0 +1,5 @@
+python ~/RecSysChallenge2024/src/polimi/scripts/nfm_training.py \
+    -output_dir /mnt/ebs_volume/models \
+    -dataset_path /mnt/ebs_volume/experiments/subsample_train_new \
+    -params_file /home/ubuntu/RecSysChallenge2024/configuration_files/best_params_nfm.json \
+    -model_name NFM_new_features_
