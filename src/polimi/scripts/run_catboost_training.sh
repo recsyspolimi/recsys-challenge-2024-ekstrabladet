70f47dd463e9ab87ac4e395040b3da4f970413dc
CarloSgaravatti <carlo.sgaravatti@mail.polimi.it>
2024-06-07 21:43:57 +0000

    bash for catboost training

--- src/polimi/scripts/run_catboost_training.sh
+++ src/polimi/scripts/run_catboost_training.sh
@@ -1,6 +1,6 @@
 python ~/RecSysChallenge2024/src/polimi/scripts/catboost_training.py \
     -output_dir /mnt/ebs_volume/models \
-    -dataset_path /mnt/ebs_volume/experiments/subsample_train_new \
-    -catboost_params_file /home/ubuntu/RecSysChallenge2024/configuration_files/catboost_baseline_new_features.json \
+    -dataset_path /mnt/ebs_volume/experiments/subsample_complete_new_with_recsys/ \
+    -catboost_params_file /home/ubuntu/RecSysChallenge2024/configuration_files/catboost_classifier_recsys_best.json \
     -catboost_verbosity 20 \
-    -model_name catboost_baseline_new_features
+    -model_name catboost_classifier_train_val_recsys
