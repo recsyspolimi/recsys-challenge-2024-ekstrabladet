758211d82b66b5ad487fc1df121b25f5b82d0399
CarloSgaravatti <carlo.sgaravatti@mail.polimi.it>
2024-06-08 21:17:05 +0000

    deepfm categorical encoder

--- src/polimi/scripts/run_deepfm_training.sh
+++ src/polimi/scripts/run_deepfm_training.sh
@@ -2,6 +2,5 @@ python ~/RecSysChallenge2024/src/polimi/scripts/deepfm_training.py \
     -output_dir /mnt/ebs_volume/models \
     -dataset_path /mnt/ebs_volume/experiments/subsample_train_new_yeo_johnson_fit \
     -params_file /home/ubuntu/RecSysChallenge2024/configuration_files/best_params_deepfm.json \
-    -early_stopping_path /home/ubuntu/experiments/early_stopping_ds \
-    -numerical_transformer_es /mnt/ebs_volume/experiments/subsample_train_new_yeo_johnson_fit/numerical_transformer.joblib \
+    -early_stopping_path /home/ubuntu/experiments/early_stopping_ds_yeo_johnson \
     -model_name deepFM_new_features
