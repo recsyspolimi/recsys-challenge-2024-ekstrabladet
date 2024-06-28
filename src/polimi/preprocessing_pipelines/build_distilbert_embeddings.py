815b8d695fcc5f8793a2b887ed239060592bcdf8
SaverioMaggese99 <126009586+SaverioMaggese99@users.noreply.github.com>
2024-06-28 10:49:22 +0200

    uploaded files to generate kenneth and titles ambeddings

---
+++ src/polimi/preprocessing_pipelines/build_distilbert_embeddings.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+import polars as pl
+import pandas as pd
+from transformers import AutoTokenizer, AutoModel
+from scipy.special import softmax
+import tqdm
+import argparse
+import torch
+import torch.nn.functional as F
+
+#Mean Pooling - Take attention mask into account for correct averaging
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+def row_to_list(row):
+    return row.tolist()
+
+def main(PATH, OUTPATH):
+    articles = pl.read_parquet(PATH)
+    
+    # Sentences we want sentence embeddings for
+    titles = articles['title'].to_list()
+    article_ids = articles['article_id'].to_list()
+
+    # Load model from HuggingFace Hub
+    tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
+    model = AutoModel.from_pretrained('distilbert-base-uncased')
+
+    # Tokenize sentences
+    encoded_input = tokenizer(titles, padding=True, truncation=True, return_tensors='pt')
+
+    # Compute token embeddings
+    with torch.no_grad():
+        model_output = model(**encoded_input)
+
+    # Perform pooling
+    embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+
+    # Normalize embeddings
+    embeddings = F.normalize(embeddings, p=2, dim=1)
+
+    distilbert_title = pd.DataFrame({
+    'embeddings': embeddings,
+    'articles_id': article_ids
+    })
+
+    distilbert_title['embeddings'] = distilbert_title.apply(lambda row: row_to_list(row), axis=1)
+
+    Path(OUTPATH).mkdir(parents=True, exist_ok=True)
+    distilbert_title.to_csv(OUTPATH  + '/distilbert_title.csv')
+
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Script for building emotions embeddingd")
+    parser.add_argument("-output_dir", default="../../experiments/", type=str,
+                        help="The directory where the embeddings will be placed")
+    parser.add_argument("-dataset_path", default=None, type=str, required=True,
+                        help="Directory of the article dataframe")
+
+    args = parser.parse_args()
+    OUTPUT_DIR = args.output_dir
+    DATASET_DIR = args.dataset_path
+    
+    main(DATASET_DIR, OUTPUT_DIR)
+
